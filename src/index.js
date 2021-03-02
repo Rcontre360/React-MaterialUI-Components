35b59e658bd1d2b93fe8e498a5b2aa6a9c403db1
Rafael Contreras <rcontreraspimentel@gmail.com>
2021-03-01 21:05:58 -0400

    added index file and package.json

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+class App extends Component {
+	render() {
+		return (
+			<div>Add reusable components to render</div>
+		);
+	}
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
